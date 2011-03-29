04ad369ca1839fb391689a89b8d21dab89830040
Ken <r2doesinc@poiteeserver.com>
2011-03-29 01:24:27 -0400

    bump to 1.2.0

--- products/version.mk
+++ products/version.mk
@@ -1,3 +1,3 @@
 #Entering version number here will update all products, all instances of version number
-build-version := 1.1.0
+build-version := 1.2.0
 product-version := $(build-name)-$(build-version) 
