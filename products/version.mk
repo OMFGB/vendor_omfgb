8abe4496fc8cd6215d245e44896b1154ac56162d
r2DoesInc <r2doesinc@poiteeserver>
2011-05-16 21:54:10 -0400

    Update to 1.3.0

--- products/version.mk
+++ products/version.mk
@@ -1,3 +1,3 @@
 #Entering version number here will update all products, all instances of version number
-build-version := 1.2.0
+build-version := 1.3.0
 product-version := $(build-name)-$(build-version) 
