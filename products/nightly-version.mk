70135fa3ca0f9fa37088075a4b6fd3cef25539d9
r2DoesInc <r2doesinc@gmail.com>
2011-06-20 19:46:07 -0400

    Change from incremental nightly number to date, MM-DD
    Solves the issue of the nightlys version needing to be manually updated.

--- products/nightly-version.mk
+++ products/nightly-version.mk
@@ -1,4 +1,4 @@
 $(call inherit-product, vendor/omfgb/products/nightly.mk)
 
-build-version := 1.3.0_nightly-11
+build-version := 1.3.0_nightly-$(shell date +%m-%d)
 product-version := $(build-name)-$(build-version)
