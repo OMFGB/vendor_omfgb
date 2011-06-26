1bf85f6d9bd5cb4a0267bbea55b4be82c3dbc2fa
xoomdev <xoomdevnet@gmail.com>
2011-06-25 19:56:35 -0500

    Clean up mecha makefile.  - Add aicbootfix and stk apps to build

--- products/omfgb_mecha.mk
+++ products/omfgb_mecha.mk
@@ -10,12 +10,13 @@ PRODUCT_BRAND := htc
 PRODUCT_DEVICE := mecha
 PRODUCT_MODEL := Thunderbolt
 PRODUCT_MANUFACTURER := HTC
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_DISPLAY_ID=$(build-version) PRODUCT_NAME=mecha BUILD_FINGERPRINT=verizon_wwe/htc_mecha/mecha/mecha:2.2.1/FRG83D/338893:user/release-keys PRODUCT_BRAND=verizon_wwe TARGET_BUILD_TYPE=user BUILD_VERSION_TAGS=release-keys PRIVATE_BUILD_DESC="1.12.605.6 CL338893 release-keys" PRODUCT_MODEL=Thunderbolt PRODUCT_MANUFACTURER=HTC
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_DISPLAY_ID=$(build-version) PRODUCT_NAME=htc_mecha BUILD_ID=FRG83D BUILD_FINGERPRINT=verizon_wwe/htc_mecha/mecha/mecha:2.2.1/FRG83D/338893:user/release-keys PRIVATE_BUILD_DESC="1.12.605.6 CL338893 release-keys"
 
-PRODUCT_PROPERTY_OVERRIDES += ro.product.version=1.12.605.6
-
-# Add the Torch app
-PRODUCT_PACKAGES += Torch
+# Add the mecha specific product packages
+PRODUCT_PACKAGES += \
+	Stk \
+	Torch \
+	AicBootFix
 
 # Broadcom FM radio
 $(call inherit-product, vendor/omfgb/products/bcm_fm_radio.mk)
