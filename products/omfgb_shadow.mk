e5c2d990232feece60fd5f07369990f67c44542b
r2DoesInc <r2doesinc@gmail.com>
2011-08-02 04:51:27 -0300

    Edited products/omfgb_shadow.mk via GitHub

--- products/omfgb_shadow.mk
+++ products/omfgb_shadow.mk
@@ -8,7 +8,7 @@ include vendor/omfgb/products/nightly-version.mk
 PRODUCT_NAME := OMFGB_shadow
 PRODUCT_BRAND := motorola
 PRODUCT_DEVICE := shadow
-PRODUCT_MODEL := DROIDXX
+PRODUCT_MODEL := DROIDX
 PRODUCT_MANUFACTURER := Motorola
 PRODUCT_BUILD_PROP_OVERRIDES := BUILD_ID=FRG83D BUILD_DISPLAY_ID=GRJ22 PRODUCT_NAME=shadow_vzw TARGET_DEVICE=cdma_shadow BUILD_FINGERPRINT=verizon/shadow_vzw/cdma_shadow/shadow:2.2.1/VZW/23.340:user/ota-rel-keys,release-keys PRODUCT_BRAND=verizon PRIVATE_BUILD_DESC="cdma_shadow-user 2.2.1 VZW 2.3.340 ota-rel-keys,release-keys" BUILD_NUMBER=2.3.340 BUILD_UTC_DATE=1289194863 TARGET_BUILD_TYPE=user BUILD_VERSION_TAGS=release-keys
 
