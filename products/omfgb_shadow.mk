a19574436fed4687a51e92f22288e7354c577908
r2DoesInc <r2doesinc@gmail.com>
2011-08-05 08:44:09 -0400

    fix fingerbpreint on shadow

--- products/omfgb_shadow.mk
+++ products/omfgb_shadow.mk
@@ -10,8 +10,7 @@ PRODUCT_BRAND := motorola
 PRODUCT_DEVICE := shadow
 PRODUCT_MODEL := DROIDX
 PRODUCT_MANUFACTURER := Motorola
-PRODUCT_BUILD_PROP_OVERRIDES := BUILD_ID=FRG83D BUILD_DISPLAY_ID=GRJ22 PRODUCT_NAME=shadow_vzw TARGET_DEVICE=cdma_shadow BUILD_FINGERPRINT=verizon/shadow_vzw/cdma_shadow/shadow:2.2.1/VZW/23.340:user/ota-rel-keys,release-keys PRODUCT_BRAND=verizon PRIVATE_BUILD_DESC="cdma_shadow-user 2.2.1 VZW 2.3.340 ota-rel-keys,release-keys" BUILD_NUMBER=2.3.340 BUILD_UTC_DATE=1289194863 TARGET_BUILD_TYPE=user BUILD_VERSION_TAGS=release-keys
-
+PRODUCT_BUILD_PROP_OVERRIDES := BUILD_ID=FRG83D BUILD_DISPLAY_ID=GRJ22 PRODUCT_NAME=shadow_vzw TARGET_DEVICE=shadow BUILD_FINGERPRINT=verizon/shadow_vzw/cdma_shadow/shadow:2.2.1/VZW/23.340:user/ota-rel-keys,release-keys PRODUCT_BRAND=verizon PRIVATE_BUILD_DESC="cdma_shadow-user 2.2.1 VZW 2.3.340 ota-rel-keys,release-keys" BUILD_NUMBER=2.3.340 BUILD_UTC_DATE=1289194863 TARGET_BUILD_TYPE=user BUILD_VERSION_TAGS=release-keys
 PRODUCT_COPY_FILES += \
 	vendor/omfgb/prebuilt/shadow/system/lib/libbridge.so:system/lib/libbridge.so \
 	vendor/omfgb/prebuilt/shadow/system/lib/libLCML.so:system/lib/libLCML.so \
